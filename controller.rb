f30060efeeacc120e27feb75525c5cf149afab06
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-02-09 15:53:07 -0600

    views added + styled, empty class and took out result route

--- controller.rb
+++ controller.rb
@@ -9,3 +9,9 @@ end
 
 #insert a route that directs to /result
 	#should render the result.erb view
+
+# get '/result' do
+# 	@result = ["You Lose", "You Win"].sample
+# 	erb :result
+# end
+
---
+++ models/yummy_rummy.rb
@@ -0,0 +1,4 @@
+class Yummy_rummy
+
+
+end
