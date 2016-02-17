cebbfedb947c9384f446408f10da6c730afb1486
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-02-17 15:28:38 -0600

    sample solution

--- controller.rb
+++ controller.rb
@@ -1,18 +1,22 @@
 #Add any requires 
 	require 'sinatra'
-
+	require_relative 'models/yummy_rummy.rb'
 
 get '/' do
 	erb :index
 end
 
+get '/result/:card' do
+	@card = params[:card]
+	@result = Yummy_rummy.get_result
+	erb :result
+end
 
 #insert a route that directs to /result
 	#should render the result.erb view
 
 
-	#wrap this inside of a route, 
-	#hint: you may need to look in the view to see what kind of route you need to setup
-	#@result = ["You Lose", "You Win"].sample
+	#wrap this inside of a route, hint: you may need to look in the view to see what kind of route you need to setup
+		#@result = ["You Lose", "You Win"].sample
 
 
--- models/yummy_rummy.rb
+++ models/yummy_rummy.rb
@@ -1,6 +1,38 @@
 class Yummy_rummy
+	@@wins = 0
+	@@rounds = 0
 
-	def get_result
-		["You Win", "You Lose"].sample
+	def self.add_round
+		@@rounds +=1
 	end
+
+	def self.too_many_wins
+		if (@@wins.to_f / @@rounds.to_f * 100) > 30
+			return true
+		else
+			return false
+		end
+	end
+
+	def self.tally_result(result)
+		if result == "You Win"
+			@@wins += 1
+		end
+	end
+
+
+	def self.get_result
+		add_round
+
+		if too_many_wins
+			return "You Lose"
+
+		else
+			result = ["You Win", "You Lose"].sample
+			tally_result(result)
+			return result
+		end
+	end
+
+
 end
